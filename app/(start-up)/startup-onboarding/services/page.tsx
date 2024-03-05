c9d9e3590532e2a5a986d4dd95fe88b37c5607f5
Bookieblue <oluwabukola.fasan@gmail.com>
2024-03-05 01:27:50 +0100

    startup-onboarding

--- app/(start-up)/startup-onboarding/services/page.tsx
+++ app/(start-up)/startup-onboarding/services/page.tsx
@@ -1,8 +1,8 @@
 "use client"
-import StartUpServicesForm from '@/components/forms/startupServices';
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import StartUpServicesForm from '@/components/forms/startUpServices';
 
 
 const page = () => {
