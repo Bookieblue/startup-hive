3c56aae072c43dcd534ffaf8927cb6ae02cf7251
Bookieblue <oluwabukola.fasan@gmail.com>
2024-03-21 11:26:20 +0100

    profile

--- app/(start-up)/profile/empty-state/page.tsx
+++ app/(start-up)/profile/empty-state/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { TRENDING_STARTUPS } from '@/app/constants';
+import { TrendingStartupItem } from '@/components/trending-startup/page';
 
 const page = () => {
   return (
@@ -78,42 +79,6 @@ const page = () => {
         </div>
       </div>
     </section>
-  );
-};
-
-type TrendingStartupItem = {
-  name: string;
-  icon: string;
-  field: string;
-  location: String;
-  description: string;
-};
-
-const TrendingStartupItem = ({
-  name,
-  icon,
-  field,
-  location,
-  description,
-}: TrendingStartupItem) => {
-  return (
-    <li className="w-full items-start mb-3 list-none">
-      <div className='flex'>
-      <div className="rounded-full p-4 lg:p-7">
-        <Image src={icon} alt="map" width={100} height={28} />
-      </div>
-      <div>
-        <div className="flex gap-2">
-          <p className="bold-18 text-white">{name}</p>
-          <p className="text-gray-40 regular-12 mt-1.5">{field}</p>
-        </div>
-        <p className="regular-14 mt-5 text-gray-40">{description}</p>
-        <p className="text-white regular-12">{location}</p>
-      </div>
-      </div>
-      <hr className='border-gray-30 mt-5'/>
-    </li>
-    
   );
 };
 
--- app/(start-up)/profile/page.tsx
+++ app/(start-up)/profile/page.tsx
@@ -1,9 +1,9 @@
-'use client';
-import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { STARTUPS, TRENDING_STARTUPS } from '@/app/constants';
+import { TrendingStartupItem } from '@/components/trending-startup/page';
+import { StartupItem } from '@/components/startup/page';
 
 const page = () => {
   return (
@@ -97,114 +97,6 @@ const page = () => {
         </div>
       </div>
     </section>
-  );
-};
-
-type TrendingStartupItem = {
-  name: string;
-  icon: string;
-  field: string;
-  location: string;
-  description: string;
-};
-
-const TrendingStartupItem = ({
-  name,
-  icon,
-  field,
-  location,
-  description,
-}: TrendingStartupItem) => {
-  return (
-    <li className="w-full items-start mb-3 list-none">
-      <div className="flex">
-        <div className="rounded-full p-4 lg:p-7">
-          <Image
-            src={icon}
-            alt="map"
-            width={100}
-            height={28}
-            className="sm:w-[80%] lg:w-full"
-          />
-        </div>
-        <div>
-          <div className="flex gap-2">
-            <p className="bold-18 text-white">{name}</p>
-            <p className="text-gray-40 regular-12 mt-1.5">{field}</p>
-          </div>
-          <p className="regular-14 mt-3 lg:mt-5 text-gray-40">{description}</p>
-          <p className="text-white regular-12">{location}</p>
-        </div>
-      </div>
-      <hr className="border-gray-30 mt-5" />
-    </li>
-  );
-};
-
-type StartupItem = {
-  name: string;
-  icon: string;
-  field: string;
-  location: String;
-  description: string;
-  supports: number;
-};
-
-const StartupItem = ({
-  name,
-  icon,
-  field,
-  location,
-  description,
-  supports,
-}: StartupItem) => {
-  const [support, setSupport] = useState(supports);
-  const [isSupport, setisSupport] = useState(false);
-
-  const SupportIcon = () => {
-    setSupport(support + (isSupport ? -1 : 1));
-    setisSupport(!isSupport);
-  };
-
-  return (
-    <li className="w-full items-start mb-3 list-none">
-      <div className="flex justify-between items-center">
-        <div className="flex">
-          <div className="rounded-full pr-3">
-            <Image src={icon} alt="map" width={50} height={28} />
-          </div>
-          <div>
-            <div className="flex gap-2">
-              <p className="bold-18 text-gray-50">{name}</p>
-              <p className="text-gray-30 regular-12 mt-1.5">{field}</p>
-            </div>
-            <p className="regular-14 mt-2 text-gray-30">{description}</p>
-            <p className="text-gray-50 regular-12">{location}</p>
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-3" onClick={SupportIcon}>
-          {isSupport ? (
-            <Image
-              src="/active-select.svg"
-              alt="map"
-              width={35}
-              height={28}
-              className="cursor-pointer"
-            />
-          ) : (
-            <Image
-              src="/non-active-select.svg"
-              alt="map"
-              width={30}
-              height={28}
-              className="cursor-pointer"
-            />
-          )}
-          <p className="regular-16">{support}</p>
-        </div>
-      </div>
-      <hr className="border-gray-40 mt-5" />
-    </li>
   );
 };
 
